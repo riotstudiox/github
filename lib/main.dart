b10d05da2128ef54685bd38517a977424a37a0af
riotstudiox <riotstudiox@gmail.com>
2025-08-10 16:42:54 +0000

    first

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  class MainApp extends StatelessWidget {
+    const MainApp({super.key});
+  
+    @override
+    Widget build(BuildContext context) {
+      return Container();
+    }
+  }
